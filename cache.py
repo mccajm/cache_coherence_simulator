83c06cffc20e4bd423401095777e6eca3d448304
Adam McCarthy <adam@adammccarthy.co.uk>
2016-02-12 21:25:11 +0000

    implemented MESI

--- cache.py
+++ cache.py
@@ -78,19 +78,42 @@ class MESICache(Cache):
 
     def __init__(self, *args, **kwargs):
         self.reset_state = "I"
-        self.state_transitions = {"R": {True: {"I": "S",
+        self.state_transitions = {"R": {True: {"I": "SE",
                                                "S": "S",
-                                               "M": "M"},
+                                               "M": "M",
+                                               "E": "E"},
                                         False: {"M": "S",
+                                                "E": "S",
                                                 "S": "S",
                                                 "I": "I"}},
                                   "W": {True: {"I": "M",
                                                "S": "M",
-                                               "M": "M"},
+                                               "M": "M",
+                                               "E": "M"},
                                         False: {"S": "I",
                                                 "M": "I",
-                                                "I": "I"}}}
+                                                "I": "I",
+                                                "E": "I"}}}
         super(MESICache, self).__init__(*args, **kwargs)
+
+    def submit_msg(self, cpu_id, op, address, shared_wire):
+        try:
+            se_flag_key = self.state_flags.index("SE")
+            if shared_wire:
+                self.state_flags[se_flag_key] = "S"
+                shared_wire = False
+            else:
+                self.state_flags[se_flag_key] = "E"
+        except ValueError:
+            pass  # no values are in SE state
+
+        is_me = (cpu_id == self.cpu_id)
+        if not is_me and op == "R":
+            index, tag = self._map_address_to_block(address)
+            if self.store[index] == tag:
+                self.shared_wire = True
+
+        super(MESICache, self).submit_msg(cpu_id, op, address)
 
 
 class MESCache(Cache):
--- main.py
+++ main.py
@@ -1,55 +1,32 @@
-from multiprocessing import Queue, Process
+from cache import MESICache
 
-from cache import MSICache
+shared_wire = False 
+def play_traceline(cache, line):
+    if line == "h":
+        print("Hit Rate P%d R:%d W:%d" % (cache.cpu_id,
+                                          cache.stats["R"]["HIT"],
+                                          cache.stats["W"]["HIT"]))
+        return
 
-
-class CPU(Process):
-    def __init__(self, bus, cache, cpu_id):
-        Process.__init__(self)
-        self.bus = bus
-        self.cache = cache(cpu_id)
-
-    def run(self):
-        while True:
-            item = self.bus.get()
-            if item == None:
-                break
-            elif item == "h":
-                print("Hit Rate P%d R:%d W:%d" % (self.cache.cpu_id,
-                                                   self.cache.stats["R"]["HIT"],
-                                                   self.cache.stats["W"]["HIT"]))
-                continue
-
-            cpu_id, op, address = item.split(" ")
-            cpu_id = int(cpu_id.lstrip("P"))
-            address = int(address, 16)
-            self.cache.submit_msg(cpu_id, op, address)
+    cpu_id, op, address = line.split(" ")
+    cpu_id = int(cpu_id.lstrip("P"))
+    address = int(address, 16)
+    cache.submit_msg(cpu_id, op, address, shared_wire)
 
 
 if __name__ == "__main__":
     num_cpus = 4
-    cpus = []
-    buses = []
+    caches = []
     for cpu_id in range(num_cpus):
-        # For simplicity, I'm allocating a queue for
-        #  each cache and replicating the trace
-        buses.append(Queue())
-        cpu = CPU(buses[cpu_id], MSICache, cpu_id)
-        cpu.start()
-        cpus.append(cpu)
-        print("Spawned CPU %d" % cpu_id)
+        caches.append(MESICache(cpu_id))
 
     print("Reading file")
     with open("trace", "r") as f:
         for line in f:
-            for bus in buses: 
-                bus.put(line)
+            for cache in caches:
+                play_traceline(cache, line)
 
     print("File read")
-    for bus in buses:
-        bus.put("h")
-        bus.put(None)
-
-    for cpu in cpus:
-        cpu.join()
+    for cache in caches:
+        play_traceline(cache, "h")
 
