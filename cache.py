4cec6318c1958275275ac5b3853285b5926bb228
Adam McCarthy <adam@adammccarthy.co.uk>
2016-02-12 15:24:31 +0000

    MSICache running

--- cache.py
+++ cache.py
@@ -1,98 +1,70 @@
-import inspect
+import math
 
 
-class CacheMissException(Exception):
-    pass
+class Cache(object):
 
-class Cache(list):
-
-    def __init__(self, block_size=4):
-        self.block_size = block_size*32  # assuming 32 bit architecture
-        self.max_value = 2**block_size
-        self.num_cache_lines = 2048/block_size
-        self.state_flags = []
-        self.stat_misses = 0
-        self.parent = parent
-        super(Cache, self).__init__(*args, **kwargs)
+    def __init__(self, cpu_id, block_size=4):
+        self.block_size = block_size  # assuming 32 bit architecture
+        self.num_cache_lines = int(2048/self.block_size)
+        self.offset_bits = int(math.log(self.block_size, 2))
+        self.index_bits = int(math.log(self.num_cache_lines, 2))
+        self.cpu_id = cpu_id
+        self.stats = {"R": {"HIT": 0,
+                            "MISS": 0,
+                            "TOTAL": 0},
+                      "W": {"HIT": 0,
+                            "MISS": 0,
+                            "TOTAL": 0}}
         self.reset()
 
-    def _check_cache_size(self):
-        if len(self) >= self.num_cache_lines:
-            raise IndexError
-
-    def _check_block_size(self, b):
-        if len(v) != self.block_size:
-            raise ValueError
-
-    def _check_new_cache_size(self, L):
-        if len(self) + len(L) > self.num_cache_lines:
-            raise IndexError
-
-    def _check_is_binary(self, b):
-        if not all((v==0 or v==1) for v in b):
-            raise ValueError
-
-    def _map_address_to_block(self, i):
-        return i
-    
-    def append(self, x):
-        raise NotImplementedError
-
-    def extend(self, L):
-        raise NotImplementedError
-
-    def insert(self, i, b):
-        self._check_block_size(b)
-        self._check_is_binary(b)
-        self._check_cache_size
-        
-        block_addr = self._map_address_to_block(b)
-        ret = super(Cache, self).insert(block_addr, x)
-        # Dirty flags are handled by child
+    def _map_address_to_block(self, address):
+        binaddr = bin(address)[2:].zfill(32)
+        offset = binaddr[-self.offset_bits:]
+        index = int(binaddr[-(self.offset_bits + self.index_bits):-self.offset_bits], 2)
+        tag = binaddr[:-(self.offset_bits + self.index_bits)]
+        return index, tag
 
     def reset(self):
-        for i in self.num_cache_lines:
-            self[i] = [-1]*self.block_size
+        try:
+            self.store = []
+            self.state_flags = []
+            for index in range(self.num_cache_lines):
+                self.store.append(None)
+                self.state_flags.append(self.reset_state)
+        except NameError:
+            raise Exception("Cache class must be subclassed with"
+                            "definitions for reset_state and"
+                            "state_transitions")
+    
+    def submit_msg(self, cpu_id, op, address):
+        is_me = (cpu_id == self.cpu_id)
+        index, tag = self._map_address_to_block(address)
+        self.stats[op]["TOTAL"] += 1
+        if self.store[index] == tag:
+            self.stats[op]["HIT"] += 1
+        else:
+            self.stats[op]["MISS"] += 1
+            self.store[index] = tag
 
-    def __add__(self, L):
-        raise NotImplementedError
+        self.state_flags[index] = \
+            self.state_transitions[op][is_me][self.state_flags[index]]
 
-    def __iadd__(self, L):
-        raise NotImplementedError
-
-    def __setslice__(self, *args, **kwargs):
-        raise NotImplementedError
-
-    def __getitem__(self, b):
-        block_addr = self._map_address_to_block(b)
-        return self.__getitem__(self, b)
-
-
-def MSICache(Cache):
+ 
+class MSICache(Cache):
 
     def __init__(self, *args, **kwargs):
-        super(MSICache, self).__init__(self, *args, **kwargs)
-        self.state_transitions = {"__getitem__": {"I": "S",
+        self.reset_state = "I"
+        self.state_transitions = {"R": {True: {"I": "S",
                                                   "S": "S",
                                                   "M": "M"},
-                                  "insert": {"I": "M",
-                                             "S": "M",
-                                             "M": "M"}}
-                                  
-
-    def insert(self, i, b):
-        super(MSICache, self).insert(i, b)
-        self.state_flags[b] = \
-            self.state_transitions[method_name][self.state_flags[b]]
-
-    def reset(self):
-        super(MSICache, self).reset()
-        self.state_flags = ["I"]*self.num_cache_lines
-
-    def __getitem__(self, b):
-        method_name = inspect.stack()[0][3]
-        self.state_flags[b] = \
-            self.state_transitions[method_name][self.state_flags[b]]
-        
-        return super(MSICache, self).__getitem__(b)
+                                           False: {"M": "S",
+                                                   "S": "S",
+                                                   "I": "I"}},
+                                  "W": {True: {"I": "M",
+                                                   "S": "M",
+                                                   "M": "M"},
+                                            False: {"S": "I",
+                                                    "M": "I",
+                                                    "I": "I"}}}
+        super().__init__(*args, **kwargs)
 
--- main.py
+++ main.py
@@ -1,32 +1,47 @@
-import threading
-
-import redis
+from threading import Thread
+from queue import Queue
 
 from cache import MSICache
 
 
-class CPU(threading.Thread):
-    def __init__(self, r, cache):
-        threading.Thread.__init__(self)
-        self.redis = r
-        self.pubsub = self.redis.pubsub()
-        self.pubsub.subscribe("cachebus")
+class CPU(Thread):
+    def __init__(self, bus, cache):
+        Thread.__init__(self)
+        self.bus = bus
         self.cache = cache
-
-    def work(self, msg):
-        print(item['data'])
+        a = 1
 
     def run(self):
-        for item in self.pubsub.listen():
-            self.work(item)
+        while True:
+            item = self.bus.get()
+            cpu_id, op, address = item.split(" ")
+            cpu_id = int(cpu_id.lstrip("P"))
+            address = int(address, 16)
+            self.cache.submit_msg(cpu_id, op, address)
+            self.bus.task_done()
 
 
 if __name__ == "__main__":
-    r = redis.Redis()
-    
     num_cpus = 4
     cpus = []
-    for i in num_cpus:
-        client = Listener(r, MSICache())
-        client.start()
+    buses = []
+    caches = []
+    for cpu_id in range(num_cpus):
+        # For simplicity, I'm allocating a queue for
+        #  each cache and replicating the trace
+        buses.append(Queue())
+        caches.append(MSICache(cpu_id))
+        cpu = CPU(buses[cpu_id], caches[cpu_id])
+        cpu.start()
+        cpus.append(cpu)
 
+    with open("trace", "r") as f:
+        for line in f:
+            for bus in buses: 
+                bus.put(line)
+
+    for cache in caches:
+        print(cache.stats)
+
+    for bus in buses:
+        bus.join()
