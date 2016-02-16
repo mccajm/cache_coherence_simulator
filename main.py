a07d235a6a0bb0376552a8c45b437478f93dda67
Adam McCarthy <adam@adammccarthy.co.uk>
2016-02-16 14:04:10 +0000

    removed global in main.py

--- main.py
+++ main.py
@@ -12,11 +12,7 @@ from cache.protocols.mes import MESCache
 from utils import int_or_None, parse_line
 
 
-stats = {"MSICache": {},
-         "MESICache": {},
-         "MESCache": {}}
-def record_stats(caches):
-    global stats
+def record_stats(stats, caches):
     for cache in caches:
         stats[cache.__class__.__name__][cache.block_size] = cache.stats
 
@@ -25,16 +21,19 @@ if __name__ == "__main__":
         lines = f.readlines()
 
     block_sizes = (2, 4, 8, 16)
+    stats = {}
     for cache in (MSICache, MESICache, MESCache):
+        cache_name = str(cache).split("'")[1].split(".")[-1]
+        stats[cache_name] = {}
         for block_size in block_sizes:
             bus = Bus(cache, 4, block_size=block_size)
 
-            print("Processing trace with %s at block size %d..." % (bus.caches[-1].__class__.__name__, block_size))
+            print("Processing trace with %s at block size %d..." % (cache_name, block_size))
             for line in tqdm(lines, leave=True):
                 line = parse_line(line)
                 bus.process_transaction(*line)
 
-            record_stats(bus.caches)
+            record_stats(stats, bus.caches)
 
     with open("stats-block_size.pkl", "wb") as f:
         pickle.dump(stats, f)
